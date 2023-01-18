34801181479a2d2a6dc3e68d4248fd1d4548fbbb
luisillo619 <luiscarlosrangel619@gmail.com>
2023-01-18 15:27:25 -0600

    Modificacion modelo Order

--- api/models/Order.js
+++ api/models/Order.js
@@ -1,11 +1,28 @@
 const { Schema, model } = require("mongoose");
 
+//AGREGAR VARIAS REFERENCIAS A LA ORDEN 
+// const Purchase = mongoose.model('Purchase', purchaseSchema);
+
+// new Order({
+//   products: [productId1, productId2, productId3],
+//   // Otros campos de la compra
+// });
+
+// purchase.save()
+//   .then(purchase => {
+//     // Compra guardada exitosamente
+//   })
+//   .catch(error => {
+//     // Error al guardar la compra
+//   });
+
+
 const orderSchema = new Schema(
   {
-    products: {
-        type: Schema.Types.ObjectId,
-        ref: 'Products'
-    },
+    products: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    }],
     Users: {
         type: Schema.Types.ObjectId,
         ref: 'User'
