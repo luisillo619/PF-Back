6bcac77a717def3607ced3c9d7c7fe54c46beea0
MarioReyes <cmario.reyesp@gmail.com>
2023-01-17 23:10:43 -0500

    Actualizacion Model User

--- api/models/User.js
+++ api/models/User.js
@@ -2,6 +2,7 @@ const { Schema, model } = require("mongoose");
 //const userinfo = require('./UserInfo.js');
 // const routines = require('./Routines.js');
 
+
 //Datos de los usuarios que van a comprar
 const userSchema = new Schema({
 
@@ -47,57 +48,8 @@ const userSchema = new Schema({
     admin: {
       type: Boolean,  
       default: false
-    },
-  //Usuario
-  docIdentity: {
-    type: Number,
-    // required: true,
-  },
-  userName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  signupDate: {
-    //fecha de registro
-    type: Date,
-    default: Date.now(),
-  },
-  lastLogin: {
-    //fecha del ultimo login
-    type: Date,
-  },
-  fav: [
-    {
-      id: {
-        type: String,
-      },
-    },
-  ],
-  feedback: {
-    type: String,
-  },
-  status: {
-    type: String,
-    enum: ["Active", "inactive"],
-    default: "Active",
-  },
-
-  //Administrador (Empresa)
-  admin: {
-    type: Boolean,
-    default: false,
-
-  },
+    }
 });
+
 
 module.exports = model("User", userSchema);
