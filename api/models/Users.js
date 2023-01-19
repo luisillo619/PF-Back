3afc5fc7b9d8b79ac82c6fd85dfe3c1677b565c5
luisillo619 <luiscarlosrangel619@gmail.com>
2023-01-19 15:35:44 -0600

    Creacion de rutas protegidas y autenticacion de token

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,57 +1,36 @@
 const { Schema, model } = require("mongoose");
 
 //Datos de los usuarios que van a comprar
-const userSchema = new Schema(
-  {
-    //Usuario
-    docIdentity: {
-      type: Number,
-      // required: true,
-    },
-    userName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    signupDate: {
-      //fecha de registro
-      type: Date,
-      default: Date.now(),
-    },
-    lastLogin: {
-      //fecha del ultimo login
-      type: Date,
-    },
-   
-
-
-    feedback: {
-      type: String,
-    },
-    status: {
-      type: String,
-      enum: ["Active", "inactive"],
-      default: "Active",
-    },
-
- address:{
-  type: Schema.Types.ObjectId,
-  ref: "Address",
- },
-    //Administrador (Empresa)
-    admin: {
-      type: Boolean,
-      default: false,
-    },
+const userSchema = new Schema({
+  //Usuario
+  docIdentity: {
+    type: Number,
+    // required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  signupDate: {
+    //fecha de registro
+    type: Date,
+    default: Date.now(),
+  },
+  lastLogin: {
+    //fecha del ultimo login
+    type: Date,
+  },
+  favorites: [{ type: Schema.Types.ObjectId, ref: 'Favorites' }],
   },
   { versionKey: false }
 );
