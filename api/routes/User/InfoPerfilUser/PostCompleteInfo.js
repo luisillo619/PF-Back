cbedbce26ae6dee9dca88353d8c1702ce887b2f8
MarioReyes <cmario.reyesp@gmail.com>
2023-01-27 12:17:16 -0500

    Actualización carpeta InfoPerfilUser

--- api/routes/User/InfoPerfilUser/PostCompleteInfo.js
+++ api/routes/User/InfoPerfilUser/PostCompleteInfo.js
@@ -3,23 +3,25 @@ const completeInfo = express.Router();
 const Users = require("../../../models/Users");
 const { isUser } = require("../../../middleware/auth");
 
+
 // Ruta para agregar los datos completos de un usuario
-completeInfo.post("/:id", isUser, async (req, res) => {
+completeInfo.use("/:id", isUser, async (req, res) => {
   try {
     const { name, lastName, docIdentity } = req.body;
     if (!name || !lastName || !docIdentity)
-      return res.status(400).json( "Faltan datos" );
-  
+      return res.status(400).json({ msg: "Faltan datos" });
+
     Users.findByIdAndUpdate(req.params.id, req.body, (error, datos) => {
       if (error) {
         res.status(500).send(error);
       } else {
         res.status(201).send(datos);
       }
-    }); 
+    });
   } catch (error) {
-    res.status(500).send('Error en el servidor');
+    res.status(500).send('Error en el servidor al buscar tus datos');
   }
-  
 });
+
+
 module.exports = completeInfo;
