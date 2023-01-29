509f55b3ef686d6801aa27cb9210c1be4c477b61
luisillo619 <luiscarlosrangel619@gmail.com>
2023-01-28 20:04:55 -0600

    LOGIN

--- api/passport.js
+++ api/passport.js
@@ -1,23 +1,30 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
-const { CLIENT_ID,CLIENT_SECRET } = process.env;
-
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
+const Users = require("./models/Users");
 //PASO 2
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-      CLIENT_ID,
+      clientID: CLIENT_ID,
       clientSecret: CLIENT_SECRET,
       callbackURL: "/auth/google/callback", // este es el que tengo que mandar a llamar en el fron para que me devuelva los datos del ususario
       scope: ["profile", "email"],
     },
-    function (accessToken, refreshToken, profile, callback) {
-      // ESTO SE EJECUTA A LA PAR DE LA RUTA CALLBACK, CUANDO SE DE CLICK EN ALGUNA CUENTA, HACE LA VERIFICACION DE SI AUTH FUE EXITOSO O NO 
-      callback(null, profile);
+    function (accessToken, refreshToken, profile, cb) {
+      Users.findOneAndUpdate(
+        { googleId: profile.id },
+        { googleId: profile.id },
+        { upsert: true, new: true },
+        function (err, user) {
+          return cb(err, user);
+        }
+      );
     }
   )
 );
+
+//ESTO SON LAS COOKIES
 
 passport.serializeUser((user, done) => {
   done(null, user);
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,5 @@
 const { Router } = require("express");
 const route = Router();
-
-
 //ADMIN
 const adminDeleteComment = require("./Admin/Comments Admin/DeleteComment");
 const adminDeleteProducts = require("./Admin/Products/DeleteProducts");
@@ -11,15 +9,11 @@ const adminDeleteUser = require("./Admin/Users/DeleteUsers");
 const adminGetUser = require("./Admin/Users/GetAllUsers");
 const adminPutLockedUser = require("./Admin/Users/PutLockedUser");
 const adminPutUser = require("./Admin/Users/PutUser");
-
-
 //ORDER
 const deleteOrder = require("./Order/DeleteOrder");
 const getOrder = require("./Order/GetOrder");
 const postOrder = require("./Order/PostOrder");
-
-
-//USER
+//USERr
 const userDeleteAddress = require("./User/Address/DeleteAddress");
 const userGetAddress = require("./User/Address/GetAddress");
 const userPostAddress = require("./User/Address/PostAddress");
@@ -29,9 +23,7 @@ const getFavorites = require("./User/Fav/GetFav");
 const postFavorites = require("./User/Fav/PostFav");
 const getAccountProfile = require("./User/InfoPerfilUser/GetAccountProfile");
 const userPostCompleteInfo = require("./User/InfoPerfilUser/PostCompleteInfo");
-const putPassword= require('./User/InfoPerfilUser/PutPassword');
-const postPassword= require('./User/InfoPerfilUser/PostPassword');
-
+//const putPassword= require('./User/Info Perfil User/PutPassword');
 
 //AMBOS
 const adminGetCategories = require("./GetCategories");
@@ -43,42 +35,31 @@ const loginGoogle = require("./loginGoogle.js");
 const postComment = require("./PostComment");
 const putComment = require("./PutComment");
 const register = require("./register");
-
-
-/*-------------------Rutas-------------------*/
+/*--------------Rutas--------------*/
 //ADMIN
 route.use("/deleteComent", adminDeleteComment);
 route.use("/adminDeleteProducts", adminDeleteProducts);
 route.use("/adminPostProducts", adminPostProducts);
 route.use("/adminPutProducts", adminPutProducts);
-
 route.use("/deleteUser", adminDeleteUser); //Si
 route.use("/getAllUsers", adminGetUser); //Si
-
 route.use("/adminPutLockedUser", adminPutLockedUser);
 route.use("/putUser", adminPutUser);
-
-
 //ORDER
 route.use("/deleteOrder", deleteOrder);
 route.use("/getOrder", getOrder);
 route.use("/postOrder", postOrder);
-
-
 //USER
 route.use("/deleteAddress", userDeleteAddress);
 route.use("/getAddress", userGetAddress);
 route.use("/postAddress", userPostAddress);
-route.use("/putAddress", userPutAddress);
+route.use("/putAddress", userPutAddress); //Si
 route.use("/deleteFav", deleteFavorites);
 route.use("/getFav", getFavorites);
 route.use("/postFav", postFavorites);
 route.use("/getAccountProfile", getAccountProfile);
 route.use("/postCompleteInfo", userPostCompleteInfo);
-route.use("/putPassword", putPassword);
-route.use("/postPassword", postPassword);
-
-
+//route.use("/putPassword", putPassword);
 //RUTAS DE AMBOS
 route.use("/adminGetCategories", adminGetCategories);
 route.use("/adminGetComment", adminGetComment);
@@ -88,7 +69,5 @@ route.use("/loginCorreo", login);
 route.use("/auth", loginGoogle);
 route.use("/postComent", postComment);
 route.use("/updateComent", putComment);
-route.use("/register", register);
-
-
+route.use("/register", register); //Si
 module.exports = route;
