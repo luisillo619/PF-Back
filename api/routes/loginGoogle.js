f8eb3a4fcdeb5d847de8f77a3f92b68402101574
MrAleew <andrespastorf@gmail.com>
2023-01-29 01:52:45 -0500

    loginFacebook

--- api/routes/loginGoogle.js
+++ api/routes/loginGoogle.js
@@ -53,7 +53,7 @@ router.get(
     failureRedirect: "/login/failed",
   })
 );
-
+// GIT
 router.get("/github", passport.authenticate("github", { scope: ["profile","email"] }));
 
 router.get(
@@ -63,7 +63,17 @@ router.get(
     failureRedirect: "/login/failed",
   })
 );
-// GIT
+// FACE
+router.get("/facebook", passport.authenticate("facebook", { scope: ["profile","email"] }));
+
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
+
 
 
 
