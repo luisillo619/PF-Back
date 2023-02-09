6e075e075624c1c18b4600c49c39987b32a3f233
MrAleew <andrespastorf@gmail.com>
2023-02-08 21:55:16 -0500

    pruebacambioapp

--- api/app.js
+++ api/app.js
@@ -24,20 +24,17 @@ app.use(passport.session());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use((req, res, next) => {
-  app.use(cors({
-    origin: '*',
-    credentials: true,
-  }));
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-
 app.use("/", routes);
 
 module.exports = app;
