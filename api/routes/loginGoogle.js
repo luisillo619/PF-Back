b14b93acf79c0bbb4f09e727bd6dfac9ae4ad1af
luisillo619 <luiscarlosrangel619@gmail.com>
2023-02-12 18:39:22 -0600

    deploy

--- api/routes/loginGoogle.js
+++ api/routes/loginGoogle.js
@@ -24,7 +24,7 @@ const generateAuthToken = (user) => {
 // req.user solo va a existir una vez, esto debido a las cookies que configuramos en la app por 1 hora
 //req.user va a existir despues de 1 hora o si se deslogea y se vuelve a logear
 router.get("/login/success", (req, res) => {
-  console.log("req.user", req)
+  // console.log("req.user", req)
 
   if (req.user) {
   //  console.log("hay un ususario en la ruta", req.user)
@@ -57,9 +57,12 @@ router.get("/google", passport.authenticate("google", { scope: ["profile","email
 router.get(
   "/google/callback",
   passport.authenticate("google", {
-    successRedirect: CLIENT_URL, //localhost
+    // successRedirect: CLIENT_URL, //localhost
     failureRedirect: "/login/failed",
-  })
+  }),(req,res)=>{
+    console.log("cebollin")
+    console.log(req)
+  }
 );
 // GIT
 router.get("/github", passport.authenticate("github", { scope: ["profile","email"] }));
